5cc191f682e5d01ffc10dfcc9e8082dd805c159f
Andre Melo <andre.vsm@hotmail.com>
2020-02-14 08:37:07 -0300

    Navigation graph added.

--- app/src/main/java/com/avsm/myanimalsapp/view/DetailFragment.kt
+++ app/src/main/java/com/avsm/myanimalsapp/view/DetailFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.avsm.myanimalsapp.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class DetailFragment : Fragment() {
 
@@ -16,6 +19,15 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnList.setOnClickListener {
+            val action = DetailFragmentDirections.actionDetailFragmentToListFragment()
+            Navigation.findNavController(it).navigate(action)
+
+        }
     }
 
 
